62911c3b9ee38cfef4d82a6d5b198574cf7437c1
derrick <telldsk@gmail.com>
2016-11-30 03:01:25 -0500

    make a video using a window's dimensions as the video dimensions

---
+++ record_window.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+
+# getting window borders
+# http://unix.stackexchange.com/a/156349
+entire=false
+x=0
+y=0
+w=0
+h=0
+b=0  # b for border
+t=0  # t for title (or top)
+
+eval $(xwininfo  |
+      sed -n -e "s/^ \+Absolute upper-left X: \+\([0-9]\+\).*/x=\1/p" \
+             -e "s/^ \+Absolute upper-left Y: \+\([0-9]\+\).*/y=\1/p" \
+             -e "s/^ \+Width: \+\([0-9]\+\).*/w=\1/p" \
+             -e "s/^ \+Height: \+\([0-9]\+\).*/h=\1/p" )
+if [ "$entire" = true ]
+then
+    extents=$(xprop _NET_FRAME_EXTENTS -id "$aw" | grep "NET_FRAME_EXTENTS" | cut -d '=' -f 2 | tr -d ' ')
+    bl=$(echo $extents | cut -d ',' -f 1) # width of left border
+    br=$(echo $extents | cut -d ',' -f 2) # width of right border
+    t=$(echo $extents | cut -d ',' -f 3)  # height of title bar
+    bb=$(echo $extents | cut -d ',' -f 4) # height of bottom border
+
+    let x=$x-$bl
+    let y=$y-$t
+    let w=$w+$bl+$br
+    let h=$h+$t+$bb
+fi
+
+echo recording "$w"x"$h" $x,$y
+
+# avconv -f alsa -i pulse -f x11grab -r 25 -s 1440x900 -i :0.0+0,0 -vcodec libx264 -pre:0 ultrafast -threads 4 $1.mkv
+
+if [ `expr ${w} % 2` -eq 1 ]; then
+    w=`expr $w + 1`;
+    echo "setting width: $w"
+fi
+
+if [ `expr ${h} % 2` -eq 1 ]; then
+    h=`expr $h + 1`;
+    echo "setting height: $h"
+fi
+
+avconv -f alsa -i pulse -f x11grab -r 25 -s ${w}x${h} -i :0.0+${x},${y} -vcodec libx264 -pre:0 ultrafast -threads 4 ${1}.mkv
