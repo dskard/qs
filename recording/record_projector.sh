d18e3e3d7435e4832c8a0135440aa513d973e8f8
derrick <telldsk@gmail.com>
2017-09-03 09:13:12 -0400

    updating base install with cert hints tweaks to projector recording script

--- recording/record_projector.sh
+++ recording/record_projector.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-#avconv -f x11grab -r 25 -s 1440x900 -i :0.0+0,0 -vcodec libx264 -pre lossless_ultrafast -threads 0 $1.mkv
-
-# can't use lossless_ultrafast with audio or we get a 6 second delay between audio and video
-
 # ./record_projector my_recording_name 1024x768
 
 if [[ "x$2" == "x" ]] ; then
