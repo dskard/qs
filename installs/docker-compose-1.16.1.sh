287f44822cfa03d08ee908575a605fc8bf7ec5aa
codedsk <telldsk@gmail.com>
2017-10-03 09:02:42 -0400

    adding install script for docker-compose 1.16.1

---
+++ installs/docker-compose-1.16.1.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# show commands being run
+set -x
+
+# Fail script on error.
+set -e
+
+pkgname=docker-compose
+VERSION=1.16.1
+basedir=/opt
+environdir=${basedir}/environ.d
+profiledir=/etc/profile.d
+pkginstalldir=${basedir}/${pkgname}
+tarinstalldir=${pkginstalldir}/tars
+installprefix=${pkginstalldir}/${VERSION}
+downloaduri="https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-`uname -s`-`uname -m`"
+script=$(readlink -f ${0})
+installdir=$(dirname ${script})
+
+if [[ ! -d ${installprefix} ]] ; then
+    mkdir -p ${installprefix}
+fi
+cd ${installprefix}
+
+if [[ ! -e ${pkgname} ]] ; then
+    curl -L ${downloaduri} > ${pkgname}
+    chmod +x ${pkgname}
+fi
+
+# setup use script
+
+cat <<- _END_ > ${profiledir}/${pkgname}-${VERSION}.sh
+export PATH=${installprefix}:${PATH}
+_END_
