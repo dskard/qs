927e72b175da0bffa353405af6bcefe3c850342e
dskard <telldsk@gmail.com>
2018-11-17 21:07:46 -0500

    adding vagrant install script

---
+++ installs/vagrant-2.1.2_install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# show commands being run
+set -x
+
+# Fail script on error.
+set -e
+
+pkgname=vagrant
+VERSION=2.1.2
+pkgname="vagrant_${VERSION}_x86_64.deb"
+downloaduri="https://releases.hashicorp.com/vagrant/${VERSION}/${pkgname}"
+script=$(readlink -f ${0})
+installdir=$(dirname ${script})
+
+curl -o ${pkgname} -L ${downloaduri}
+gdebi -n ${pkgname}
+rm ${pkgname}
+
