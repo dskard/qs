ff532f551fba6bd306c4a146124b1491a536aa51
dskard <telldsk@gmail.com>
2019-03-08 16:27:51 -0500

    add script to convert mkv to animated gif

---
+++ recording/mkgif.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Usage:
+# ./mkgif.sh input.mkv output.gif
+
+# helpful links:
+# * https://www.ostechnix.com/create-animated-gif-ubuntu-16-04/
+
+# the flags:
+#  -ss : indicates the starting point of GIF
+#  -i : input file
+#  $1 : My video file name
+#  -to : End position of the GIF file
+#  -r : frame rate. You can increase the value to get more quality GIF file
+#  -vf : filter graph. To scale the GIF image in the desired size.
+#  $2 : output file
+
+
+ffmpeg \
+    -ss 00:00:00 \
+    -i $1 \
+    -r 3 \
+    -vf scale=800:-1 \
+    $2
