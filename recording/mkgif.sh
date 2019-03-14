0c839833bf9b905aac36cce762be9fe73c7192b9
dskard <telldsk@gmail.com>
2019-03-14 15:37:36 -0400

    better quality gifs using palettes

--- recording/mkgif.sh
+++ recording/mkgif.sh
@@ -5,6 +5,8 @@
 
 # helpful links:
 # * https://www.ostechnix.com/create-animated-gif-ubuntu-16-04/
+# * https://superuser.com/a/556031 (better quality, smaller size, custom palette)
+# * https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/ (better explanation, 1-line command for it all)
 
 # the flags:
 #  -ss : indicates the starting point of GIF
@@ -17,10 +19,21 @@
 #  $2 : output file
 
 
+# generate the custom palette
+ffmpeg \
+    -y \
+    -ss 00:00:00 \
+    -i $1 \
+    -vf fps=10,scale=800:-1:flags=lanczos,palettegen \
+    palette.png
+
+# output the gif using the palette
 ffmpeg \
     -ss 00:00:00 \
     -i $1 \
-    -r 3 \
-    -vf scale=800:-1 \
+    -i palette.png \
     -loop -1 \
+    -filter_complex "fps=10,scale=800:-1:flags=lanczos[x];[x][1:v]paletteuse" \
     $2
+
+rm palette.png
