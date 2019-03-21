21c1aef7f554ea32e7927beb09c98a11e153d342
dskard <telldsk@gmail.com>
2019-03-21 08:57:10 -0400

    add pyenv install script

---
+++ installs/pyenv_install.sh
@@ -0,0 +1,14 @@
+# https://github.com/pyenv/pyenv-installer
+# https://github.com/pyenv/pyenv#basic-github-checkout
+
+curl https://pyenv.run | bash
+
+sudo bash -c "cat >/etc/profile.d/pyenv.sh" <<-'_END_'
+export PATH=${HOME}/.pyenv/bin:$PATH
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+_END_
+
+# install libffi-dev for python versions 3.7.2 and up
+sudo apt-get install -y libffi-dev
+
