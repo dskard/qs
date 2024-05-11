f93969032136d7f4c3c253a7bb0cff46c196f3cb
dskard <telldsk@gmail.com>
2024-05-10 21:52:17 -0400

    updating justfile install script

--- installs/just-latest_install.sh
+++ installs/just-latest_install.sh
@@ -1,9 +1,12 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-# show commands being run
-set -x
+set -euxo pipefail
 
-# Fail script on error.
-set -e
+# setup the package repo
+# https://docs.makedeb.org/prebuilt-mpr/getting-started/#setting-up-the-repository
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update
 
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/local/bin/
+# https://github.com/casey/just?tab=readme-ov-file#packages
+sudo apt install just fzf
